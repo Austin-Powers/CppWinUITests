66be2c678c3010beedcb1b8127923e50e63b1451
thomas <thomas.schlott@gmx.de>
2022-08-11 00:32:24 +0200

    Created basic structure for development

---
+++ simulation/simulation.h
@@ -0,0 +1,12 @@
+#ifndef CPPWINUITESTS_SIMULATION_H
+#define CPPWINUITESTS_SIMULATION_H
+
+namespace APowers {
+
+/// @brief Class for performing a particle simulation on a grid.
+class Simulation final
+{};
+
+} // namespace APowers
+
+#endif // !CPPWINUITESTS_SIMULATION_H
---
+++ simulation_tests/simulation.cpp
@@ -0,0 +1,5 @@
+#include "simulation/simulation.h"
+
+#include <gtest/gtest.h>
+
+namespace APowers::UnitTests {}
